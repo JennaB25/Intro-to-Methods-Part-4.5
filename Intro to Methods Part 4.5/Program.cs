8dc23bbd03a818a3f93b9f00c2cd817653cab779
362700239 <jennarbloor@gmail.com>
2023-02-15 10:55:10 -0500

    Add project files.

---
+++ Intro to Methods Part 4.5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Intro_to_Methods_Part_4._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to draw some pictures?");
+            Console.ReadLine();
+            drawCow();
+            drawPig();
+            drawBird();
+        }
+        //Part One ASCII Art
+        public static void drawCow()
+        {
+            Console.WriteLine("((...))");
+            Console.WriteLine("( O O )");
+            Console.WriteLine(" [   ] ");
+            Console.WriteLine(" (`_`) ");
+            Thread.Sleep(500);
+        }
+        public static void drawPig()
+        {
+            Console.WriteLine("    _____  ");
+            Console.WriteLine("^..^     \\9");
+            Console.WriteLine("(oo)_____/ ");
+            Console.WriteLine("   WW  WW");
+            Thread.Sleep(500);
+        }
+        public static void drawBird()
+        {
+            //finish
+            Thread.Sleep(500);
+        }
+    }
+}
