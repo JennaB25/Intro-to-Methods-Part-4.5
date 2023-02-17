5ebb724ae735d8a734c861062e0f01e8afa47d10
362700239 <jennarbloor@gmail.com>
2023-02-17 10:41:11 -0500

    worked on x and y positions

--- Intro to Methods Part 4.5/Program.cs
+++ Intro to Methods Part 4.5/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Data;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Intro_to_Methods_Part_4._5
@@ -16,22 +18,24 @@ namespace Intro_to_Methods_Part_4._5
             inputX = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Y - Position:");
             inputY = Convert.ToInt32(Console.ReadLine());
-            Console.SetCursorPosition(inputX, inputY);        
-                drawCow();           
-                drawPig();           
-                drawBird();           
-            Console.SetCursorPosition(0, inputY + 19);
+            Console.SetCursorPosition(inputX, inputY);
+                drawCow();
+                drawPig();
+                drawBird();
+            Console.Clear();
             Console.WriteLine("Now do you want to hear a joke?");
             Console.ReadLine();
             knockKnockJoke();
-            
+            Console.Clear();
         }
         //Part One: ASCII Art
         public static void drawCow()
         {
-            Console.WriteLine("((...))");
-            Console.WriteLine("( O O )");
-            Console.WriteLine(" [   ] ");
+            var (x, y) = Console.GetCursorPosition();
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine("((...))");            
+            Console.WriteLine("( O O )");            
+            Console.WriteLine(" [   ] ");           
             Console.WriteLine(" (`_`) ");
             Console.WriteLine("");
             Thread.Sleep(500);
